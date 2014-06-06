d46488ac42f380419f41547ee4e1ebee74af6699
luto <m@luto.at>
2014-06-06 16:02:18 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='hoerapi',
+      version='1.0',
+      description='',
+      author='luto',
+      author_email='m@luto.at',
+      url='https://github.com/hoersuppe/hoerapi.py',
+      packages=['hoerapi'],
+     )
